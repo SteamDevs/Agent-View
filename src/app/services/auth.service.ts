22bbee912ae9f77e5d3d358930aa43fa650c0730
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-04-07 21:53:45 -0600

    save token in localstorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,6 +9,8 @@ import { environment } from '../../environments/environment'
 })
 export class AuthService {
 
+  private _user  : User;
+  private _token : string;
   public httpHeaders = new HttpHeaders({
     'Content-Type': 'application/x-www-form-urlencoded',
     'Authorization': 'Basic ' + environment.APP_CREDENTIALS
@@ -23,6 +25,30 @@ export class AuthService {
     let params = `grant_type=${ environment.APP_CONSSECION }&username=${ user.username }&password=${ user.password }`
  
     return this.http.post<any>(environment.API_URL_OAUTH, params , { headers: this.httpHeaders })
+  }
+
+  saveUser(accessToken: string): void {
+    let payload = this.getDataToken(accessToken);
+    this._user = new User;
+    this._user.nombre = payload.nombre;
+    this._user.apellido = payload.apellido;
+    this._user.email = payload.email;
+    this._user.username = payload.user_name;
+    this._user.roles = payload.authorities;
+    sessionStorage.setItem('usuario', JSON.stringify(this._user));
+  }
+
+  saveToken(accessToken: string) : void {
+    this._token = accessToken;
+    sessionStorage.setItem('token', accessToken);
+  }
+
+  getDataToken(accessToken: string) : any {
+    if (accessToken != null) {
+      return JSON.parse(atob(accessToken.split(".")[1]));
+    }
+
+    return null;
   }
 
 }
