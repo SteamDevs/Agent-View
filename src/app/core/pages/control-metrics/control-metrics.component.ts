42321df19715aee0c4e0665a390c996a5444b8f8
Pablo Jacobo <pablojacobo1998@gmail.com>
2019-04-12 10:19:28 -0600

    Implementes metrics view monitoring

--- src/app/core/pages/control-metrics/control-metrics.component.ts
+++ src/app/core/pages/control-metrics/control-metrics.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControlMetricsComponent implements OnInit {
 
+  public lineChartData: Array<any> = [
+    { data: [0, 0, 0, 0 ], label: 'Monitoring' },
+    { data: [0, 0, 0, 0 ], label: 'Monitoring' }
+  ];
+
+  public lineChartLabels: Array<any> = ['Performance', 'latency', 'integrity', 'Trafic' ];
+
   constructor() { }
 
   ngOnInit() {
+  
+    setInterval( ()=>{
+
+      let MetricData =  [
+
+        Math.round(  Math.random() + 40 ),
+        Math.round(  Math.random() + 10 ),
+        Math.round(  Math.random() + 80 ),
+        Math.round(  Math.random() + 10 )
+      ]
+
+      let InnerMetrics = [
+
+        Math.round(  Math.random()  ),
+        Math.round(  Math.random() + 10 ),
+        Math.round(  Math.random() + 70  ),
+        Math.round(  Math.random() + 20 )
+      
+      ]
+
+      this.lineChartData  = [
+        {data : InnerMetrics, label: 'Main process' },
+        {data : MetricData, label: 'Child Processes' }
+      ]
+
+    }, 1000)
+  
   }
 
 }
